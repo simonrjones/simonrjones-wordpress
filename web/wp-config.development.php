c361982fb4e538b55cec54dbf0340dea58dee6ff
Simon R Jones <simon@studio24.net>
2016-01-01 11:43:20 +0000

    Updating local dev db

--- web/wp-config.development.php
+++ web/wp-config.development.php
@@ -14,7 +14,7 @@
 // ** MySQL settings - You can get this info from your web host ** //
 
 /** The name of the database for WordPress */
-define('DB_NAME', 'simonrjones');
+define('DB_NAME', 'simonrjones_dev');
 
 /** MySQL database username */
 define('DB_USER', 'test-user');
